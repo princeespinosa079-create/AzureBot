8c3bde223019accbb6845d34383a9c43df9af8ba
AzureHub <princeespinosa079@gmail.com>
2026-04-24 11:12:59 +0800

    Initialize Discord bot with basic setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
